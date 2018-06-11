218cf53c35d8a3d0690a0a821219621d84f1ced9
pmontalb <paolomontalbano61@gmail.com>
2018-06-11 22:15:36 +0100

    Added additional test with transport equation exact solution

--- UnitTests/FiniteDifferenceTests.cpp
+++ UnitTests/FiniteDifferenceTests.cpp
@@ -142,4 +142,45 @@ namespace pdet
 			}
 		}
 	}
+
+	TEST_F(FiniteDifferenceTests, SineSolutionNoDiffusion)
+	{
+		cl::vec grid = cl::LinSpace(0.0f, 1.0f, 10);
+		auto _grid = grid.Get();
+
+		std::vector<float> _initialCondition(10);
+		for (unsigned i = 0; i < _initialCondition.size(); ++i)
+			_initialCondition[i] = sin(_grid[i]);
+
+		cl::vec initialCondition(_initialCondition);
+		
+		unsigned steps = 10;
+		double dt = 1e-4;
+		float velocity = .05f;
+		float diffusion = 0.0f;
+
+		float finalTime = steps * dt;
+		std::vector<float> _exactSolution(10);
+		for (unsigned i = 0; i < _initialCondition.size(); ++i)
+			_exactSolution[i] = sin(_grid[i] - velocity * finalTime);
+
+		for (const SolverType solverType : enums::IterableEnum<SolverType>())
+		{
+			pde::GpuSinglePdeInputData data(initialCondition, grid, velocity, diffusion, dt, solverType);
+			pde::sol1D solver(data);
+
+			solver.Advance(steps);
+			const auto solution = solver.solution->columns[0]->Get();
+
+			for (size_t i = 1; i < solution.size() - 1; ++i)  // excluding BC
+			{
+				//if (fabs(solution[i] - _exactSolution[i]) > 1.1e-4)
+				//{
+				//	int a = 0;
+				//}
+				ASSERT_TRUE(fabs(solution[i] - _exactSolution[i]) <= 1.1e-4);
+			}
+			
+		}
+	}
 }
